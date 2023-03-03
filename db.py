d3ed42b14cf4d7eb7bc49ca628d50928164fa6b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-03 15:26:34 +0300

    Moved the MetaData import and definition to the db script

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 """This file holds the logic that helps the file interact with the database"""
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 engine = create_engine("mysql+pymysql://root@localhost:3306/fastapirefresher")
+
+meta = MetaData()
 
 conn = engine.connect()
--- models/index.py
+++ models/index.py
@@ -1,6 +1,4 @@
 """The index file for the models"""
-from sqlalchemy import MetaData
 from models.user import user
 
-meta = MetaData()
 
