b482bdc549b226163ab835a94f2d62be4f341404
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-03 15:30:33 +0300

    Created the users table with four columns

--- models/user.py
+++ models/user.py
@@ -2,3 +2,11 @@
 """
 from sqlalchemy import Table, Column
 from db import meta
+
+users = Table(
+    'users', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(255)),
+    Column('email', String(255)),
+    Column('password', String(255)),
+)
