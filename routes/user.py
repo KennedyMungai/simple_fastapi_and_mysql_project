72271f067972849a04341024448427fbac4fbae4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-03 15:46:07 +0300

    Added some more api endpoints to the script

--- routes/user.py
+++ routes/user.py
@@ -1,8 +1,30 @@
 from fastapi import APIRouter
 from db import conn
 from models.index import users
+from schemas.index import User
 
 user = APIRouter()
+
+@user.get('/')
+async def read_data():
+    return conn.execute(users.select()).fetchall()
+
+@user.get('/{id}')
+async def read_data(id: int):
+    return conn.execute(users.select().where(users.c.id == id)).fetchall()
+
+@user.post('/')
+async def write_data(user: User):
+    conn.execute(users.insert().values(
+            name=user.name,
+            email=user.email,
+            password = user.password
+        ))
+    return conn.execute(users.select()).fetchall()
+
+@user.get('/')
+async def read_data():
+    return conn.execute(users.select()).fetchall()
 
 @user.get('/')
 async def read_data():
