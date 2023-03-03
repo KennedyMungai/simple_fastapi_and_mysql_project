b8f0d44cbecdd4affa27c455e9a4200db7d74f39
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-03 15:16:58 +0300

    Created an instance if the create_engine function called engine

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
 """This file holds the logic that helps the file interact with the database"""
 
 from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://root@localhost:3306/fastapirefresher")
